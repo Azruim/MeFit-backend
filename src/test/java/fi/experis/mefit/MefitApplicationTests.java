343db35b0c0ab3fe80f0173f31cb149c0bddfc85
Nico Behnen <Azruim@live.fi>
2022-03-07 12:19:55 +0200

    chore: project initialisation.

---
+++ src/test/java/fi/experis/mefit/MefitApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.experis.mefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MefitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
