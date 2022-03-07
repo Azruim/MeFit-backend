ac35fd4d4a3635df571eb824df69fd4af2126be0
Sasu Korhonen <smage90@hotmail.com>
2022-03-07 12:37:55 +0200

    feat: Set entity

--- src/main/java/fi/experis/mefit/models/Set.java
+++ src/main/java/fi/experis/mefit/models/Set.java
@@ -1,4 +1,18 @@
 package fi.experis.mefit.models;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 public class Set {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column
+    private int exercise_repetitions;
+
+
 }
