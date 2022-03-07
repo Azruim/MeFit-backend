a72079ec0d4a3299445ae69120538db62df837cd
Sasu Korhonen <smage90@hotmail.com>
2022-03-07 12:35:17 +0200

    feat: Address entity

--- src/main/java/fi/experis/mefit/models/Address.java
+++ src/main/java/fi/experis/mefit/models/Address.java
@@ -1,4 +1,33 @@
 package fi.experis.mefit.models;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long address_id;
+
+    @Column
+    private String address_line_1;
+
+    @Column
+    private String address_line_2;
+
+    @Column
+    private String address_line_3;
+
+    @Column
+    private String postal_code;
+
+    @Column
+    private String city;
+
+    @Column
+    private String country;
+
 }
