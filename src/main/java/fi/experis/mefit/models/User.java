32810b986f38aa7377fd254c64208bd4d0f85c99
Sasu Korhonen <smage90@hotmail.com>
2022-03-07 12:33:28 +0200

    feat: User entity

--- src/main/java/fi/experis/mefit/models/User.java
+++ src/main/java/fi/experis/mefit/models/User.java
@@ -1,4 +1,30 @@
 package fi.experis.mefit.models;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+
+    @Column
+    private String password;
+
+    @Column
+    private String first_name;
+
+    @Column
+    private String last_name;
+
+    @Column
+    private boolean isContributor;
+
+    @Column
+    private boolean isAdmin;
+
 }
