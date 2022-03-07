bc1455a1841b4267c22d2bf07983f2249f23a327
Sasu Korhonen <smage90@hotmail.com>
2022-03-07 12:39:57 +0200

    feat: Profile entity

--- src/main/java/fi/experis/mefit/models/Profile.java
+++ src/main/java/fi/experis/mefit/models/Profile.java
@@ -1,4 +1,26 @@
 package fi.experis.mefit.models;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long profile_id;
+
+    @Column
+    private double weight;
+
+    @Column
+    private double height;
+
+    @Column
+    private String medical_conditions;
+
+    @Column
+    private String disabilities;
 }
