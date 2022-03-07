0bc987329659ed396997ec56783157e471ac572d
Sasu Korhonen <smage90@hotmail.com>
2022-03-07 12:36:42 +0200

    feat: Exercise entity

--- src/main/java/fi/experis/mefit/models/Exercise.java
+++ src/main/java/fi/experis/mefit/models/Exercise.java
@@ -1,4 +1,29 @@
 package fi.experis.mefit.models;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 public class Exercise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long exercise_id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private String target_muscle_group;
+
+    @Column
+    private String image;
+
+    @Column
+    private String vid_link;
 }
