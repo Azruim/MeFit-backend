22f6195f6a0133376d2f9712619ccfa2c3e6617c
Sasu Korhonen <smage90@hotmail.com>
2022-03-08 10:39:59 +0200

    feat: initial_data.sql added

---
+++ src/main/resources/initial_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO exercise (description, image, name, target_muscle_group, vid_link)
+    VALUES ('Penkki treeni', null, 'Pena', 'Rintalihakset', null),
+           ('Hauis treeni', null, 'Haukka', 'Hauislihakset', null),
+           ('Jalka treeni', null, 'Kyykky', 'Reisilihakset', null);
+
+
