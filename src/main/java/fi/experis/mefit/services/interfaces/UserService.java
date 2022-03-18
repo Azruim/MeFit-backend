09b6eb341cfe311622f91c4924b11b72821ed200
Sasu Korhonen <smage90@hotmail.com>
2022-03-18 09:19:22 +0200

    chore: deleted unused classes

--- src/main/java/fi/experis/mefit/services/interfaces/UserService.java
+++
@@ -1,8 +0,0 @@
-package fi.experis.mefit.services.interfaces;
-
-import fi.experis.mefit.models.User;
-import org.springframework.http.ResponseEntity;
-
-public interface UserService {
-    ResponseEntity<Object> updateUser(User user, String token);
-}
