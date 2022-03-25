37e1d8c6e7ad42782010b2b92d1ce41b27a2a0e0
Nico Behnen <Azruim@live.fi>
2022-03-25 12:57:27 +0200

    feat: added get dto for profile.

--- src/main/java/fi/experis/mefit/controllers/ProfileController.java
+++ src/main/java/fi/experis/mefit/controllers/ProfileController.java
@@ -1,8 +1,8 @@
 package fi.experis.mefit.controllers;
 
+import fi.experis.mefit.models.dtos.getDtos.ProfileGetDTO;
 import fi.experis.mefit.models.dtos.postDtos.CreateProfileDTO;
 import fi.experis.mefit.models.dtos.postDtos.UpdateProfileDTO;
-import fi.experis.mefit.models.entities.Profile;
 import fi.experis.mefit.services.interfaces.ProfileService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -26,7 +26,7 @@ public class ProfileController {
 
     @Operation(summary = "Get profile by id")
     @GetMapping("/{profileId}")
-    public ResponseEntity<Profile> getProfileById(@Parameter(description = "Id of profile to be searched") @PathVariable String profileId) {
+    public ResponseEntity<ProfileGetDTO> getProfileById(@Parameter(description = "Id of profile to be searched") @PathVariable String profileId) {
         return profileService.getProfileById(profileId);
     }
 
